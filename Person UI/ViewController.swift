188ccaa37445ef0463d1a0195bd5f4a92f747273
leonidesh <37727113+leonidesh@users.noreply.github.com>
2019-02-19 23:43:38 +0800

    Add files via upload

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by 刘洋 on 2019/2/16.
+//  Copyright © 2019年 Yang Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
